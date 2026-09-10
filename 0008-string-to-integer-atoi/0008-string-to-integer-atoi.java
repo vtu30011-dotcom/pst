a1de11dcaf5756777710b5e7db3a91c46dd4dd1b
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-10 11:13:52 +0530

    Time: 1 ms (100%), Space: 43.8 MB (39.39%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        int sign = 1;
+        if (i < n && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (i < n && s.charAt(i) == '+') {
+            i++;
+        }
+        int result = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return result * sign;
+    }
+}
