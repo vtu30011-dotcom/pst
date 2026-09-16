7f1525882329aedac906d393b4b2b7c04d9a0566
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-16 11:47:19 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ javainterface-java-interface-Hackerrank/javainterface-java-interface.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class MyCalculator implements AdvancedArithmetic {
+
+    public int divisor_sum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
+class Solution{
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
