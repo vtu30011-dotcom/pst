674b5bc4c866cda20e5f943727b8b3f5f2c812a4
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-16 11:23:21 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 11960-java-method-overriding-Hackerrank/11960-java-method-overriding.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+void getNumberOfTeamMembers(){
+}
+
+public class Solution{
+    
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+}
+     System.out.println("Each team has 11 players in Soccer Class");
+      }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
