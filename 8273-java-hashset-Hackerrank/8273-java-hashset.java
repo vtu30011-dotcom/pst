8324c4af6b415d22ef64ef07338a844721691ccc
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-16 11:53:47 +0530

    Force committed solution - Time: O(t), Space: O(t)

---
+++ 8273-java-hashset-Hackerrank/8273-java-hashset.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+HashSet<String> set = new HashSet<>();
+
+for (int i = 0; i < t; i++) {
+    set.add(pair_left[i] + " " + pair_right[i]);
+    System.out.println(set.size());
+}
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
