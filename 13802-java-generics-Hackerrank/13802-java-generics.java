bd9a0404505485580b3129c3ad8fd701d710196b
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-16 11:20:17 +0530

    Force committed solution - Time: O(N + M), Space: O(N + M)

---
+++ 13802-java-generics-Hackerrank/13802-java-generics.java
@@ -0,0 +1,31 @@
+public class Solution {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+
+class Printer { public <T> void printArray(T[] array) { for (T element : array) { System.out.println(element); } } }
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
