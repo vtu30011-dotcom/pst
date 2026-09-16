72efb80a98d8d4d3b19d75a224a5477f291460a3
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-16 11:16:53 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7953-java-inheritance-ii-Hackerrank/7953-java-inheritance-ii.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+class Arithmetic {
+    
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+    
+}
+
+
+
+public class Solution{
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
