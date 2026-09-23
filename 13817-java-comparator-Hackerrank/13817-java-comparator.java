538d13a2f8795f6cdb248c4b22cf5c5be5d6d264
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-23 10:49:44 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 13817-java-comparator-Hackerrank/13817-java-comparator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score != b.score) {
+            return b.score - a.score;
+        }
+        return a.name.compareTo(b.name);
+    }
+}
+class Player{
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
