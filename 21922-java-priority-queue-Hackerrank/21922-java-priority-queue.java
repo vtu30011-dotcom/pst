9c29ea8eca0213007d26ac8fd9f74ffe79559a81
vtu30011-dotcom <vtu30011@veltech.edu.in>
2026-09-23 11:04:35 +0530

    Force committed solution - Time: O(N log N), Space: O(N)

---
+++ 21922-java-priority-queue-Hackerrank/21922-java-priority-queue.java
@@ -0,0 +1,45 @@
+} else {
+                if (!pq.isEmpty()) {
+                    pq.poll();
+                }
+            }
+        }
+
+        List<Student> result = new ArrayList<Student>();
+
+        while (!pq.isEmpty()) {
+            result.add(pq.poll());
+        }
+
+        return result;
+    }
+}
+
+public class Solution {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
+    
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());    
+        List<String> events = new ArrayList<>();
+        
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+        
+        List<Student> students = priorities.getStudents(events);
+        
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st: students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
